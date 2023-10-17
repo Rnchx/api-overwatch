99579f0fa719b0e9a6821ef4c0a085582451aace
João Pedro Rocha <jpcrocha8@gmail.com>
2023-10-17 16:19:42 -0300

    add classnames in proprieties in folder Agent.jsx

--- app/components/agent/Agent.jsx
+++ app/components/agent/Agent.jsx
@@ -3,14 +3,12 @@ import styles from './agent.module.css';
 
 const Agent = ({ key, name, portrait, description, role, location, hitpoints, abilities }) => {
     return (
-        <div key={key}>
-            <h1>{name}</h1>
+        <div key={key} className={styles.containerAgent}>
+            <h1 className={styles.nameAgent}>{name}</h1>
             <img src={portrait} alt={name} height={350} width={350} />
-            <p>{description}</p>
-            <p>{role}</p>
-            <p>Local de Origem: {location}</p>
-            
-            
+            <p className={styles.descriptionAgent}>{description}</p>
+            <p className={styles.roleAgent}>{role}</p>
+            <p className={styles.locationAgent}>Local de Origem: {location}</p>p
 
         </div>
     )
