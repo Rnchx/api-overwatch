247383e7cf372863b432d006ff36108608f6e4be
Belaalcantara <isabelaalcantara.senai564@gmail.com>
2023-10-26 08:38:50 -0300

    feat: finish ListaAgente.js/ agentes.js

---
+++ models/Agents.js
@@ -0,0 +1,19 @@
+class Agents {
+    constructor(nome, descricao, pontosDeVida, armadura, vida, escudos, especialidade, localizacao, habilidades) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.pontosDeVida = pontosDeVida;
+        this.armadura= armadura;
+        this.vida = vida;
+        this.escudos =escudos;
+        this.especialidade = especialidade;
+        this.localizacao =  localizacao;
+        this.habilidades =habilidades;
+        this.id= generateId()
+    }
+    
+    generateId(){
+        return Math.floor(Math.random() * 10000);
+    }
+}
+export default Agents;
