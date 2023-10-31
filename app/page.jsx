27ccab6c2366c159529231773f3e2e0015bb6ac4
João Pedro Rocha <jpcrocha8@gmail.com>
2023-10-31 10:34:20 -0300

    alterations inicial page

--- app/page.jsx
+++ app/page.jsx
@@ -1,39 +1,113 @@
 'use client'
-import overwatch from '@/data/overwatchClass';
+import React from 'react';
 import { useEffect, useState } from 'react';
-import Agent from './components/agent/Agent';
 import styles from './page.module.css';
 import Link from 'next/link';
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import ImageLoading from './components/imageLoading/ImageLoading';
-import Form from './components/register/register';
+import AgentTTTT from "@/models/Agents";
+import ListAgente from "@/models/listAgente";
+import overwatch, { agent } from "@/data/overwatchClass"
+import Agent from './components/agent/Agent';
+import { Sanchez } from 'next/font/google';
 
+const instanciaListaAgentes = new ListAgente()
 export default function Home() {
 
-  const [apiDataO, setApiDataO] = useState(null);
-  const [apiDataA, setApiDataA] = useState(null);
-  
+  const [apiData, setApiData] = useState(null);
+
+  const [listaAgentes, setListaAgentes] = useState([]);
+
+  const [nome, setNome] = useState(null);
+  const [portraitAgent, setPortraitAgent] = useState("")
+  const [descricao, setDescricao] = useState("");
+  const [armadura, setArmadura] = useState("");
+  const [vida, setVida] = useState("");
+  const [escudo, setEscudo] = useState("");
+  const [especialidade, setEspecialidade] = useState("");
+  const [nacionalidade, setNacionalidade] = useState("");
+  const [habilidade1, setHabilidade1] = useState("");
+  const [habilidade2, setHabilidade2] = useState("");
+  const [habilidade3, setHabilidade3] = useState("");
+  const [btnEdit, setBtnEdit] = useState([]);
 
   useEffect(() => {
-    const overwatchFletch = async () => {
+    console.log('rodou useefec')
+    const fetchData = async () => {
+      try {
+        const data = await overwatch();
+        console.log('data dentro do useefe', data)
+        setApiData(data);
+      } catch (error) {
 
-      const datasO = await overwatch();
-      setApiDataO(datasO);
+      }
     };
-    overwatchFletch();
-  }, [])
+    fetchData()
+  }, []);
+
+  useEffect(() => {
+    console.log('Entrou no use que preeche a class')
+    if (apiData) {
+      apiData.forEach((agenteData) => {
+        const novoAgente = new AgentTTTT(
+          agenteData.nome,
+          agenteData.portraitAgent,
+          agenteData.descricao,
+          agenteData.armadura,
+          agenteData.vida,
+          agenteData.escudo,
+          agenteData.especialidade,
+          agenteData.nacionalidade,
+          agenteData.habilidade1,
+          agenteData.habilidade2,
+          agenteData.habilidade3,
+        );
+        instanciaListaAgentes.addAgente(novoAgente);
+      });
+
+      const atualizarAgentes = [...listaAgentes, ...instanciaListaAgentes.getList()];
+      setListaAgentes(atualizarAgentes);
+    }
+  }, [apiData]);
+
+  const addAgent = () => {
+    const novoAgente = new AgentTTTT(nome, portraitAgent, descricao, armadura, vida, escudo, especialidade, nacionalidade, habilidade1, habilidade2, habilidade3);
+
+
+    if (!listaAgentes.some(agente => agente.nome === nome)) {
+      const novosAgentes = [...listaAgentes, novoAgente];
+      setListaAgentes(novosAgentes);
+    }
+
+    instanciaListaAgentes.addAgente(novoAgente);
+
+    setNome("");
+    setPortraitAgent("");
+    setDescricao("");
+    setArmadura("");
+    setVida("");
+    setEscudo("");
+    setEspecialidade("");
+    setHabilidade1("");
+    setHabilidade2("");
+    setHabilidade3("");
+  };
+
+  const removeAgent = (agente) => {
+    instanciaListaAgentes.removeAgente(agente)
+    setListaAgentes(instanciaListaAgentes.getList())
+  }
 
 
   return (
-    apiDataO ? (
+    apiData ? (
       <div className={styles.divBody}>
-
         <Header />
         <div className={styles.divBody}>
           <div className={styles.smallCard}>
             <div className={styles.card1}>
-              {apiDataO.map((agent) => (
+              {apiData.map((agent) => (
                 <Link href={`eachAgent/${agent.key}`}>
                   <div className={styles.card2}>
                     <Agent
@@ -48,18 +122,127 @@ export default function Home() {
 
               )
               )}
+              <ul>
+            {
+              listaAgentes && listaAgentes.length > 0 ? (
+                listaAgentes.map((agent) => (
+                  <div key={agent.id} className={styles.newCard}>
+                    <li className={styles.agentName}>{agent.name}</li>
+                    <img
+                      className={styles.newImg}
+                      src={agent.portrait}
+                      alt={agent.name}
+                    />
+                    <p>{agent.description}</p>
+                    <p>{agent.habilidade1}</p>
+                    <p>{agent.habilidade2}</p>
+                    <p>{agent.habilidade3}</p>
+                    <p>{agent.vida}</p>
+                    <p>{agent.armadura}</p>
+                    <p>{agent.escudo}</p>
+                    <button onClick={() => removeAgent(agent)}>Remover</button>
+                  </div>
+                ))
+              ) : (
+                <span></span>
+              )
+            }
+          </ul>
             </div>
           </div>
         </div>
         <div id={styles.formCreateAgent}>
-                <Form/>
+          <>
+            <h2>Crie seu novo agente abaixo!</h2>
+            <input
+              className={styles.inputs}
+              type="text"
+              placeholder="nome do agente"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
+
+            <select id={styles.especialdiadesAgents}>
+              <option value={agent.especialidade}>suporte</option>
+              <option value={agent.especialidade}>dano</option>
+              <option value={agent.especialidade}>tanque</option>
+            </select>
+
+            <input
+              className={styles.inputs}
+              type="text"
+              placeholder="URL da imagem"
+              value={portraitAgent}
+              onChange={(e) => setPortraitAgent(e.target.value)}
+            />
+
+            <input
+              className={styles.inputs}
+              type="text"
+              placeholder="descrição do Agente"
+              value={descricao}
+              onChange={(e) => setDescricao(e.target.value)}
+            />
+
+            <h3>Habilidades:</h3>
+
+            <input
+              className={styles.inputs}
+              type="text"
+              placeholder="nome da habilidade 1"
+              value={habilidade1}
+              onChange={(e) => setHabilidade1(e.target.value)}
+            />
+
+            <input
+              className={styles.inputs}
+              type="text"
+              placeholder="nome da habilidade 2"
+              value={habilidade2}
+              onChange={(e) => setHabilidade2(e.target.value)}
+            />
+
+            <input
+              className={styles.inputs}
+              type="text"
+              placeholder="nome da habilidade 3"
+              value={habilidade3}
+              onChange={(e) => setHabilidade3(e.target.value)}
+            />
+
+            <h3>Informações de vida.</h3>
+
+            <input
+              className={styles.inputs}
+              type="number"
+              placeholder="quantidade de vida"
+              value={vida}
+              onChange={(e) => setVida(e.target.value)}
+            />
+
+            <input
+              className={styles.inputs}
+              type="number"
+              placeholder="quantidade de armadura"
+              value={armadura}
+              onChange={(e) => setArmadura(e.target.value)}
+            />
+
+            <input
+              className={styles.inputs}
+              type="number"
+              placeholder="quantidade de escudo"
+              value={escudo}
+              onChange={(e) => setEscudo(e.target.value)}
+            />
+
+            <button onClick={addAgent}>Adicionar Agente!</button>
+          </>
         </div>
         <Footer />
       </div>
     ) : (
-      <>
-        <ImageLoading />
-      </>
+      <ImageLoading />
     )
-  )
-}
+  );
+};
